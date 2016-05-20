a785a6ab9a55476628f03765a8c4cde27ecfb230
Patrick Poh <patrickpoh91@gmail.com>
2016-05-20 22:55:48 +0800

    update webpack

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var TodoApp = require('TodoApp');
 //Load foundation-sites
 // require('style!css!foundation-sites/dist/foundation.min.css')
 // require('foundation-sites/dist/foundation.min.css')
@@ -11,6 +12,6 @@ $(document).foundation();
 require('applicationStyles')
 
 ReactDOM.render(
-  <p>Boilerplate 3 Project</p>,
+  <TodoApp/>,
   document.getElementById('app')
 );
